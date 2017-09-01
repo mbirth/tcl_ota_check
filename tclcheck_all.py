3a2025c63ad574b972c904261bba125500e73cde
Markus Birth <mbirth@gmail.com>
2017-09-01 17:59:05 +0200

    Added script to check for all PRDs from prds.txt.

---
+++ tclcheck_all.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# pylint: disable=C0111,C0326
+
+import tcllib
+import sys
+from requests.exceptions import RequestException
+
+fc = tcllib.FotaCheck()
+fc.serid = "3531510"
+fc.fv = "AAM481"
+#fc.osvs  = "7.1.1"
+fc.mode = fc.MODE_FULL
+fc.cltp  = 10
+#fc.cltp  = 2010
+
+print("List of latest {} firmware by PRD:".format("FULL" if fc.mode == fc.MODE_FULL else "OTA"))
+
+with open("prds.txt", "r") as afile:
+    prdx = afile.read()
+    prds = list(filter(None, prdx.split("\n")))
+for prdline in prds:
+    prd, model = prdline.split(" ", 1)
+    try:
+        fc.reset_session()
+        fc.curef = prd
+        check_xml = fc.do_check()
+        curef, fv, tv, fw_id, fileid, fn, fsize, fhash = fc.parse_check(check_xml)
+        print("{}: {} ({})".format(prd, tv, model))
+    except (SystemExit, RequestException):
+        print("{} ({}) failed.".format(prd, model))
+        continue
