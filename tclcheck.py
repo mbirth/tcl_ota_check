72448afd471167cbcec16e1129e00f9ad07f37ce
Markus Birth <mbirth@gmail.com>
2017-09-23 23:31:32 +0200

    New PRDs. A few optimisations in tclcheck.py.

--- tclcheck.py
+++ tclcheck.py
@@ -8,22 +8,23 @@ import random
 import sys
 
 fc = tcllib.FotaCheck()
+fc.cltp  = 10
+fc.serid = "3531510"
+#fc.osvs  = "7.1.1"
+
 if len(sys.argv) == 3:  # python tclcheck.py $PRD $FV = OTA delta for $PRD from $FV
     fc.curef = sys.argv[1]
     fc.fv = sys.argv[2]
     fc.mode  = fc.MODE_OTA
 elif len(sys.argv) == 2:  # python tclcheck.py $PRD = FULL for $PRD
     fc.curef = sys.argv[1]
-    fc.fv = "AAM481"
+    fc.fv = "AAA000"
     fc.mode = fc.MODE_FULL
+    fc.cltp  = 2010
 else:  # python tclcheck.py = OTA for default PRD, FV
     fc.curef = "PRD-63117-011"
     fc.fv    = "AAM481"
     fc.mode  = fc.MODE_OTA
-fc.serid = "3531510"
-#fc.osvs  = "7.1.1"
-fc.cltp  = 10
-#fc.cltp  = 2010
 
 check_xml = fc.do_check()
 print(fc.pretty_xml(check_xml))
